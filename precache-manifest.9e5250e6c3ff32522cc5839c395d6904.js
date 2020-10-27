fa42c298df1a27752af620ea7c2a69108b4e5dc1
lan1018 <lan1018>
2020-10-27 18:20:33 +0800

    Updates

---
+++ precache-manifest.9e5250e6c3ff32522cc5839c395d6904.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8080ec13ecf028d01e3539105ba154fb",
+    "url": "/2N-mid-208410364-project/index.html"
+  },
+  {
+    "revision": "c0729bbae21875b55996",
+    "url": "/2N-mid-208410364-project/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5f224196ddd33cae51a2",
+    "url": "/2N-mid-208410364-project/static/css/main.eaf6cdf8.chunk.css"
+  },
+  {
+    "revision": "c0729bbae21875b55996",
+    "url": "/2N-mid-208410364-project/static/js/2.ad50feb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2N-mid-208410364-project/static/js/2.ad50feb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f224196ddd33cae51a2",
+    "url": "/2N-mid-208410364-project/static/js/main.d603d3e4.chunk.js"
+  },
+  {
+    "revision": "dba06935afed5042030c",
+    "url": "/2N-mid-208410364-project/static/js/runtime-main.0a29dd5e.js"
+  },
+  {
+    "revision": "5de72de39a6f730a457f1f3db4555f68",
+    "url": "/2N-mid-208410364-project/static/media/1.5de72de3.PNG"
+  },
+  {
+    "revision": "67a9113f2e648b8db006972c760e0b66",
+    "url": "/2N-mid-208410364-project/static/media/10.67a9113f.PNG"
+  },
+  {
+    "revision": "ce52dd850ff3ca8cc31172a367e4228e",
+    "url": "/2N-mid-208410364-project/static/media/2.ce52dd85.PNG"
+  },
+  {
+    "revision": "06da98f86acc73203cfa7e070bdcdb27",
+    "url": "/2N-mid-208410364-project/static/media/3.06da98f8.PNG"
+  },
+  {
+    "revision": "20e3301953bd92a675afbe435660377c",
+    "url": "/2N-mid-208410364-project/static/media/4.20e33019.PNG"
+  },
+  {
+    "revision": "46b3c16003a8d2462fc06b7f1fbcf58a",
+    "url": "/2N-mid-208410364-project/static/media/5.46b3c160.PNG"
+  },
+  {
+    "revision": "367dbc3da309c416345e3d0681dd2924",
+    "url": "/2N-mid-208410364-project/static/media/6.367dbc3d.PNG"
+  },
+  {
+    "revision": "ed24d2fc5f423c1c46af52f4f776ebc0",
+    "url": "/2N-mid-208410364-project/static/media/7.ed24d2fc.PNG"
+  },
+  {
+    "revision": "22b006541d21a9437b4ca1511ad19ea7",
+    "url": "/2N-mid-208410364-project/static/media/8.22b00654.PNG"
+  },
+  {
+    "revision": "c6a3a188ad44bc6f60124989214d37c9",
+    "url": "/2N-mid-208410364-project/static/media/9.c6a3a188.PNG"
+  },
+  {
+    "revision": "932a10c5e3e7bbd2419c1b48dad5e943",
+    "url": "/2N-mid-208410364-project/static/media/bg1.932a10c5.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lan1018/2N-mid-208410364-project/precache-manifest.86da64c0f1d893f229a85bbc023979db.js"
+  "/2N-mid-208410364-project/precache-manifest.9e5250e6c3ff32522cc5839c395d6904.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lan1018/2N-mid-208410364-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2N-mid-208410364-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
